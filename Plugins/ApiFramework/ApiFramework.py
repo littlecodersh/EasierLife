405ed7c26ce9da19c3c37f0120ad344bf4687918
LittleCoder <i7meavnktqegm1b@qq.com>
2016-04-27 11:23:39 +0800

    ApiFramework

---
+++ Plugins/ApiFramework/ApiFramework.py
@@ -0,0 +1,34 @@
+__all__ = [ApiFramework]
+
+__API_LIST = {
+    'document':{
+        'get': 'Get document',
+    },
+}
+class ApiError(AttributeError):
+    def __init__(self, path):
+        self.message = "api has no attribute '%s'"%'.'.join(path)
+        self.args = (self.message,)
+class ApiFramework(object):
+    def __init__(self, apiList = {}, warning = False, path = []):
+        self.apiList = apiList
+        self.warning = apiList != {} and warning
+        self.path = path
+    def __getattr__(self, s):
+        try:
+            tmpApi = self.apiList
+            for name in self.path + [s]: tmpApi = tmpApi[name]
+        except:
+            if self.warning: raise ApiError(self.path + [s])
+            api = ApiFramework(self.apiList, self.warning, self.path + [s])
+        else:
+            api = ApiFramework(self.apiList, self.warning, self.path + [s])
+            api.__doc__ = tmpApi
+        return api
+    def __call__(self, *args, **kwargs):
+        return self.path
+
+if __name__ == '__main__':
+    api = ApiFramework(__API_LIST, True)
+    print(api.document.get())
+    print(api.document.send())
---
+++ Plugins/ApiFramework/README.md
@@ -0,0 +1,16 @@
+#ApiFramework 160427
+    Aimed at:
+        Create api in a quiet easy way
+    Environment:
+        Windows 8.1 - 64
+        Python 2.7.10
+    Attention:
+        Pay attention to the setting of api list
+
+    目标：
+        简单快速，创建api
+    环境：
+        Windows 8.1 - 64
+        Python 2.7.10
+    注意事项：
+        注意api list的创建
