1ad2eba11f0fb4f86ce09515cb2209270862ffc3
LittleCoder <i7meavnktqegm1b@qq.com>
2016-05-14 14:44:13 +0800

    Add thread in downloader

--- Scripts/WechatArticlePicDownloader.py
+++ Scripts/WechatArticlePicDownloader.py
@@ -1,11 +1,16 @@
 #coding=utf8
-import requests, re, os
+import requests, re, os, thread, time
 from Tkinter import Tk, Button, Entry
 import tkMessageBox
 
+TASK_STORAGE = []
+
 def download_pic_from_url(url, picDir):
     content = requests.get(url).content
-    postfix = re.compile('wx_fmt=([a-z]+)').search(url).groups()[0]
+    try:
+        postfix = re.compile('wx_fmt=([a-z]+)').search(url).groups()[0]
+    except:
+        postfix = 'jpg'
     with open((picDir+'.'+postfix).encode('cp936'), 'wb') as f: f.write(content)
 
 def get_pic_url_from_url(url):
@@ -37,12 +42,17 @@ def button_clicked():
     if not title:
         tkMessageBox.showinfo('Warning', u'检测到非微信文章页面')
         return
+    thread.start_new_thread(download, (url, title, picUrl))
+    tkMessageBox.showinfo('Info', u'已经成功加入队列')
+
+def download(url, title, picUrl):
     for sk in (r'\/:*?"<>|'): title = title.replace(sk, '')
+    title = title.replace('&nbsp;', ' ')
     if not os.path.exists(title.encode('cp936')): os.mkdir(title.encode('cp936'))
     download_pic_from_url(picUrl, os.path.join(title, u'标题图'))
     for i, picUrl in enumerate(get_pic_url_from_url(url), 1):
         download_pic_from_url(picUrl, os.path.join(title, str(i)))
-    tkMessageBox.showinfo('Info', u'读取成功，请打开"%s"文件夹查看'%title)
+    TASK_STORAGE.append(title)
 
 mainWindow = Tk()
 button = Button(mainWindow, cnf = {'command': button_clicked, 'text': 'Download', 'justify': 'right', })
@@ -50,4 +60,10 @@ inputEntry = Entry(mainWindow, width=70)
 inputEntry.pack()
 button.pack()
 mainWindow.title(u'微信图片下载器')
-mainWindow.mainloop()
+while 1:
+    mainWindow.update_idletasks()
+    mainWindow.update()
+    try:
+        tkMessageBox.showinfo('Info', u'读取成功，请打开"%s"文件夹查看'%TASK_STORAGE.pop())
+    except:
+        pass
