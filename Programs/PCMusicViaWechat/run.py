9eabde652bbda2214ece204cf88596b88f727c11
LittleCoder <i7meavnktqegm1b@qq.com>
2016-09-27 09:44:55 +0800

    Simplify the demo

--- Programs/PCMusicViaWechat/run.py
+++ Programs/PCMusicViaWechat/run.py
@@ -1,25 +1,31 @@
 #coding=utf8
-import itchat
-from Lib.InteractMusicApi import interact_select_song, close_music
+import os
 
-HELP_MSG = '''\
+import itchat
+from NetEaseMusicApi import interact_select_song
+
+HELP_MSG = u'''\
 欢迎使用微信网易云音乐
 帮助： 显示帮助
 关闭： 关闭歌曲
 歌名： 按照引导播放音乐\
 '''
 
-@itchat.msg_register('Text')
+with open('stop.mp3', 'w') as f: pass
+def close_music():
+    os.startfile('stop.mp3')
+
+@itchat.msg_register(itchat.content.TEXT)
 def music_player(msg):
     if msg['ToUserName'] != 'filehelper': return
     if msg['Text'] == u'关闭':
         close_music()
-        return '音乐已关闭'
+        itchat.send(u'音乐已关闭', 'filehelper')
     if msg['Text'] == u'帮助':
-        return HELP_MSG
+        itchat.send(HELP_MSG, 'filehelper')
     else:
-        return interact_select_song(msg['Text'])
+        itchat.send(interact_select_song(msg['Text']), 'filehelper')
 
-itchat.auto_login(True, enableCmdQR = True)
-itchat.send(HELP_MSG) 
+itchat.auto_login(True, enableCmdQR=True)
+itchat.send(HELP_MSG, 'filehelper') 
 itchat.run()
